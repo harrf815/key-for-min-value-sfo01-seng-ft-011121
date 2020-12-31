8c8f6204fb7bc32c271948a7bd3b45880298dba0
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-31 00:08:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 0
+  empty_hash = nil 
+  
+  name_hash.each do |product, price|
+    if price < min_value
+      
+      min_value = price
+      return min_value
+    end
+    
+  end
+   min_value
+  empty_hash
 end
